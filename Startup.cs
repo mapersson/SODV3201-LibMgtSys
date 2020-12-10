9182c01db76d1a7936c6305fa59120383a8cf948
mapersson <62900241+mapersson@users.noreply.github.com>
2020-12-10 11:03:24 -0700

    Update Startup.cs
    Added Identity services to the Startup configuration.

--- Startup.cs
+++ Startup.cs
@@ -9,8 +9,10 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore.Diagnostics;
 using SODV3201_LibMgtSys.Data;
+using SODV3201_LibMgtSys.Models;
 
 namespace SODV3201_LibMgtSys
 {
@@ -31,6 +33,7 @@ namespace SODV3201_LibMgtSys
                 options.UseSqlite(Configuration.GetConnectionString("LibraryContext"))
             );
             services.AddDatabaseDeveloperPageExceptionFilter();
+            services.AddIdentity<AppUser, IdentityRole>().AddEntityFrameworkStores<LibraryContext>().AddDefaultTokenProviders();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
